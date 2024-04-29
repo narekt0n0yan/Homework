fe2e74d531e430991e425d88ac7f6c00d2db7112
Narek <narektonoyan142@gmail.com>
2024-04-29 15:57:43 +0400

    ...

---
+++ AddBorder.py
@@ -0,0 +1,23 @@
+def solution(picture):
+    matrix = []
+    first_row = '*' * (len(picture[0]) + 2)
+    matrix.append(first_row)
+    for i in picture:
+        row = "*"
+        row += i
+        row += '*'
+        matrix.append(row)
+    matrix.append(first_row)
+    return matrix
+
+
+
+print(solution( ["abc",
+                 "ded"]))
+
+a = "abc"
+a = list(a)
+
+print(a)
+
+            
--- alternatingSums.py
+++ alternatingSums.py
@@ -1,5 +1,13 @@
 def solution(a: list):
+    output = []
     first_team = 0
     for i in a[::2]:
-        
+        first_team += i
+    second_team = 0
+    for j in a[1::2]:
+        second_team += j
+    output.append(first_team)
+    output.append(second_team)
+    return output
+
     
