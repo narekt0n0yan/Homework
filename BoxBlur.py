d2aea8c0dea4240e010f9b20e64e52a9d6609ca3
Narek <narektonoyan142@gmail.com>
2024-05-18 13:18:47 +0400

    ...

--- BoxBlur.py
+++ BoxBlur.py
@@ -1,9 +1,9 @@
 from ZeroMatrix import zeromatrix
 def solution(image: list):
     matrix_list = []
-    for _ in range(len(image)-2): 
+    for _ in range(len(image) - 2): 
         a = []       
-        for __ in range(len(image[0])-2):
+        for __ in range(len(image[0]) - 2):
             a.append(0)
         matrix_list.append(a)
     
@@ -16,22 +16,22 @@ def solution(image: list):
                                  + image[i + 1][j] 
                                  + image[i + 1][j + 2] 
                                  + image[i + 2][j + 2]
-                                + image[i + 2][j] 
-                                + image[i][j + 2] 
-                                + image[i][j]) // 9
+                                 + image[i + 2][j] 
+                                 + image[i][j + 2] 
+                                 + image[i][j]) // 9
     return matrix_list
 
     
-print(solution([[36,0,18,9,9,45,27], 
-                [27,0,54,9,0,63,90], 
-                [81,63,72,45,18,27,0], 
-                [0,0,9,81,27,18,45], 
-                [45,45,27,27,90,81,72], 
-                [45,18,9,0,9,18,45], 
-                [27,81,36,63,63,72,81]]))
+print(solution([[36, 0, 18, 9, 9, 45, 27], 
+                [27, 0, 54, 9, 0, 63, 90], 
+                [81, 63, 72, 45, 18, 27, 0], 
+                [0, 0, 9, 81, 27, 18, 45], 
+                [45,  45, 27, 27, 90, 81, 72], 
+                [45, 18, 9, 0, 9, 18, 45], 
+                [27, 81, 36, 63, 63, 72, 81]]))
 
-print(solution([[7,4,0,1], 
-                [5,6,2,2], 
-                [6,10,7,8], 
-                [1,4,2,0]]))
+print(solution([[7, 4, 0, 1], 
+                [5, 6, 2, 2], 
+                [6, 10, 7, 8], 
+                [1, 4, 2, 0]]))
 
