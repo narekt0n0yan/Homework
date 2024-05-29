a95230acef0acc268d2b573fdadb9fe9d6f67698
Narek <narektonoyan142@gmail.com>
2024-05-29 22:16:37 +0400

    ...

--- Binar.Tree.py
+++ Binar.Tree.py
@@ -2,7 +2,7 @@ class Node():
     def __init__(self, value):
         self.right = None
         self.left  = None
-        self.value = value
+        self.value = value 
 
 
 
@@ -12,19 +12,53 @@ class BinarTree():
     def __init__(self, head):
         self.root = head
     
-    def add(self, new_int):
-        a = self.root
-        while a is not None:
-            if new_int > a:
-                a = self.left
-            a = self.right
-        if new_int > a:
-            new_int = self.left
-        new_int = self.right
-            
+    def add(self, new_int: int) -> bool:
+        current_node: Node = self.root
+        while current_node is not None:
+            if current_node.value > new_int:
+                if current_node.left is None:
+                    break
+                else:
+                    current_node = current_node.left
+            elif current_node.value < new_int:
+                if current_node.right is None:
+                    break
+                else:
+                    current_node = current_node.right
+            else:
+                return False
+        if new_int > current_node.value:
+            current_node.right = Node(new_int)
+        else:
+            current_node.left = Node(new_int)
+        return True
+    
+    
+    def delete(self, value: int) -> bool:
+        current_node: Node = self.root
+        prev_node: Node = None
+        if value == current_node.value:
+            return False
+        while current_node != value:
+            if current_node.value > value:
+                prev_node = current_node
+                current_node = current_node.left
+            elif current_node < value:
+                prev_node = current_node
+                current_node = current_node.right
+        prev_node.left = current_node.right
+        left = current_node.left
+        current_node = current_node.right
+        while current_node.left is not None:
+            current_node = current_node.left
+        current_node.left = left
+
+        
+        
+
 
 l = BinarTree(Node(10))
-# l.add(Node(6))
+l.add(6)
 print(l)
         
 
--- SingleLinkedList_Class.py
+++ SingleLinkedList_Class.py
@@ -2,7 +2,7 @@ class Node():
     def __init__(self, value):
         self.value = value
         self.next = None
-
+        
 
 class SingleLinkedList():
     def __init__(self, head=None):
